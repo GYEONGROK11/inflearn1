130ab763307427969e228abbb37e880b27d6b2cd
gpol2300 <gpol123@naver.com>
2024-03-24 21:11:23 +0900

    인프런 스프링 입문

--- src/main/java/com/spring/spring1/service/MemberService.java
+++ src/main/java/com/spring/spring1/service/MemberService.java
@@ -18,7 +18,7 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
-    //회원가입 2
+    //회원가입
     public Long join(Member member) {
         validateDuplicateMember(member); //중복이름x
         //저장
